965d9c7943e4986272dfb0f7fab91f1d929e3350
Ana Laura Bonetti Kraus <analaurabonettikraus@gmail.com>
2026-03-25 16:58:28 -0300

    database

--- api/db.php
+++ api/db.php
@@ -8,7 +8,7 @@ function conectar() {
     return new PDO(
         "mysql:host=localhost;dbname=architech;charset=utf8",
         "root",
-        "Dandara3209",
+        "",
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
