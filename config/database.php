b53308f1a59afd2770ca5ec594f78b4266ba8a82
Dandara Franz <franzdandara@gmail.com>
2026-06-24 08:57:23 -0300

    editor novo

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'Dandara3209');
 define('DB_NAME', 'architech');
 define('DB_CHARSET', 'utf8mb4');
 
